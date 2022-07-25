0aedfc7117135e124f91e1b623f20f63a3f21cf3
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-07-25 10:25:12 +0300

    import styles to js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from "lodash";
+import 'bootstrap';
+import './styles/styles.scss';
 
-export const forTest = () => {
-  return 0;
-};
+// Dummy function for test checking. Will be removed later
+export const forTest = () => 0;
 
 const component = () => {
   const element = document.createElement("div");
-  element.innerHTML = _.join(["Hello ", "Webpack"]);
+  element.innerHTML = _.join(["Hello", " Webpack"]);
+  element.classList.add('container');
   return element;
 };
 
