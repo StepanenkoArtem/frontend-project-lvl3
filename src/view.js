e829f139b51015c89192c574606c9bb399462fc6
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2022-07-30 15:39:44 +0300

    Form validation (#6)
    * add render in the separate file
    
    * add yup validation to the form
    
    * add yup and onChange to dependencies
    
    * add autofocus and required attribute
    
    * add dummy function to tests

---
+++ src/view.js
@@ -0,0 +1,22 @@
+import { VALIDATION } from "./index";
+
+export default (path, current) => {
+  const input = document.querySelector('[aria-label = "url"]');
+  switch (path) {
+    case "form.validation": {
+      current === VALIDATION.PASSED
+        ? input.classList.remove("is-invalid")
+        : input.classList.add("is-invalid");
+      break;
+    }
+    case "form.error": {
+      break;
+    }
+    case "urls": {
+      input.classList.remove("is-invalid");
+      input.value = "";
+      break;
+    }
+    default:
+  }
+};
