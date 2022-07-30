97b1fb45607ee54a4c18eab8dfe8a4dd5b906ce3
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-07-30 15:36:55 +0300

    add yup validation to the form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
-import 'bootstrap';
-import './styles/styles.scss';
+import "bootstrap";
+import "./styles/styles.scss";
+import onChange from "on-change";
+import { object, string } from "yup";
+import render from "./view";
 
-// Dummy function for test checking. Will be removed later
-export const forTest = () => 0;
+let urlFormSchema = object({
+  url: string().url().required("URL CAN'T BE EMPTY"),
+});
 
-document.body.appendChild();
+export const VALIDATION = {
+  PENDING: "pending",
+  PASSED: "passed",
+  FAILED: "failed",
+};
+
+const form = document.querySelector("form");
+
+const initState = {
+  form: {
+    validation: VALIDATION.PENDING,
+    error: null,
+  },
+  urls: [],
+};
+
+const state = onChange(initState, render);
+
+const isNotExist = (url) =>
+  state.urls.includes(url)
+    ? new Promise((resolve, reject) => reject(new Error("Url already exists")))
+    : true;
+
+const handleValidationError = (error) => {
+  state.form.error = error.message;
+  state.form.validation = VALIDATION.FAILED;
+};
+
+const save = (url) => {
+  state.urls.push(url);
+  state.form.validation = VALIDATION.PASSED;
+  state.form.error = null;
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(form));
+  const { url } = formData;
+
+  urlFormSchema
+    .validate(formData)
+    .then(() => isNotExist(url))
+    .then(() => save(url))
+    .catch(handleValidationError);
+};
+
+form.addEventListener("submit", handleSubmit);
