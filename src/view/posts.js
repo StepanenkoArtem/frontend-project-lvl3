168d56f11711f6ff8371ed1c9b5354469808b44e
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2022-08-23 17:11:39 +0300

    Sort by date (#13)
    * remove unusable constants
    
    * add refetcher and update other handlers
    
    * add refetche to init. Update init state
    
    * render sorted

--- src/view/posts.js
+++ src/view/posts.js
@@ -1,12 +1,17 @@
+import { sortBy } from "lodash/collection";
+
 const postContainer = document.querySelector(".posts");
 
 export default (posts) => {
   postContainer.innerHTML = "";
-  posts.forEach((post) => {
-    const newPost = document.createElement("a");
-    newPost.setAttribute("href", post.link);
-    newPost.classList.add("mb-1", "row");
-    newPost.textContent = post.title;
-    postContainer.appendChild(newPost);
-  });
+
+  sortBy(posts, "pubDate")
+    .reverse()
+    .forEach((post) => {
+      const newPost = document.createElement("a");
+      newPost.setAttribute("href", post.link);
+      newPost.classList.add("mb-1", "row");
+      newPost.textContent = post.title;
+      postContainer.appendChild(newPost);
+    });
 };
