01a28a71cbb6504daf69dbe425fbebd65c667ba3
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2022-09-03 18:12:32 +0300

    Fix state (#22)
    * move state from separate file to init.js. Move state hendlers to helpers file
    
    * fix init state and buildinf url

--- src/init.js
+++ src/init.js
@@ -2,12 +2,22 @@ import onChange from 'on-change';
 import _ from 'lodash';
 import submit from './handlers/submitHandler';
 import render from './handlers/render';
-import initState from './initializers/state';
-import { DELAY } from './constants';
+import { DELAY, STATUS } from './constants';
 import modalWindow from './templates/modal.html';
 import download from './handlers/downloader';
 import parse from './handlers/parser';
 import save from './handlers/saver';
+
+const initState = {
+  error: null,
+  status: STATUS.IDLE,
+  urls: [],
+  pendingUrls: [],
+  rss: {
+    feeds: [],
+    posts: [],
+  },
+};
 
 const refreshFeeds = (state) => {
   const { pendingUrls, urls } = state;
