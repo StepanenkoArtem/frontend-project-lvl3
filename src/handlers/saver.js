5b1b62eab169dd57f2958cc0073cbcc6c4db42d3
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-09-13 19:06:08 +0300

    move handle modal window to controller

--- src/handlers/saver.js
+++ src/handlers/saver.js
@@ -10,13 +10,13 @@ export default ({ feedInfo, feedPosts }, state) => {
 
   if (!urls.includes(url)) {
     rss.feeds = [...rss.feeds, { id: feedId, ...feedInfo }];
-
-    const preparedPosts = feedPosts
-      .filter(({ guid }) => !existedPosts.map((post) => post.guid).includes(guid))
-      .map((post) => ({
-        feedId, ...post, id: nanoid(6), pubDate: Date.parse(post.pubDate),
-      }));
-
-    rss.posts = [...rss.posts, ...preparedPosts];
   }
+
+  const preparedPosts = feedPosts
+    .filter(({ guid }) => !existedPosts.map((post) => post.guid).includes(guid))
+    .map((post) => ({
+      feedId, ...post, id: nanoid(6), pubDate: Date.parse(post.pubDate),
+    }));
+
+  rss.posts = [...rss.posts, ...preparedPosts];
 };
